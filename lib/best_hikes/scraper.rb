ef3ba70485130750b554113b26ff8692f95a2599
Wendy Hamilton <wdhamilton117@gmail.com>
2019-09-23 21:50:58 +0000

    working on dumb scraper

--- lib/best_hikes/scraper.rb
+++ lib/best_hikes/scraper.rb
@@ -2,10 +2,10 @@ class BestHikes::Scraper
   
   def self.scrape_states
     doc = Nokogiri::HTML(open("https://www.outsideonline.com/2393036/50-best-hikes-us"))
-    array_of_states = doc.css("div.article__body h2")
-    array_of_states.each do |state|
-      state.text
-   end
+    array_states = doc.css("div.article__body h2").children.text
+    array_states.each do |state|
+      state
+    end
   end
   
 end
