9ab9746042b64a6cf79f01fc6707359bc00e7ace
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-01 15:04:59 +0000

    tweaking menu

--- lib/best_hikes/cli.rb
+++ lib/best_hikes/cli.rb
@@ -29,21 +29,31 @@ class BestHikes::CLI
     end 
   end
   
-  def menu
-    input = nil 
-    while input != "exit"
-    puts "\nEnter the number a state to see the top hiking destination!"
-    puts "(Enter list to view states again or exit to leave at anytime)"
-    input = gets.strip
-    index = input.to_i
-    
-    if index.between?(1,50) 
-      @hikes = BestHikes::Hikes.all
+  def show_hike_info(index)
+     @hikes = BestHikes::Hikes.all
       the_hike = @hikes[index-1]
       puts "\n#{the_hike.name}"
       @description = BestHikes::Information.all 
       the_description = @description[index-1]
       puts "\n#{the_description.description}"
+  end
+  
+  def menu
+    input = nil 
+    while input != "exit"
+    puts "\nEnter the number a state to see the top hiking destination!"
+    puts "(Enter list to view states again or exit to leave at anytime)"
+    input = gets.strip.downcase
+    index = input.to_i
+    
+    if index.between?(1,50) 
+      show_hike_info(index)
+      # @hikes = BestHikes::Hikes.all
+      # the_hike = @hikes[index-1]
+      # puts "\n#{the_hike.name}"
+      # @description = BestHikes::Information.all 
+      # the_description = @description[index-1]
+      # puts "\n#{the_description.description}"
     elsif input == "list"
       list_states
     elsif input == "exit"
