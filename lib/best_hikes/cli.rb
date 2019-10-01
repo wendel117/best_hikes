e182070ecfe417275f1bd09714f4b0708a83093b
Wendy Hamilton <wdhamilton117@gmail.com>
2019-10-01 14:58:25 +0000

    finished cli structure

--- lib/best_hikes/cli.rb
+++ lib/best_hikes/cli.rb
@@ -32,7 +32,7 @@ class BestHikes::CLI
   def menu
     input = nil 
     while input != "exit"
-    puts "Enter the number a state to see the top hiking destination!"
+    puts "\nEnter the number a state to see the top hiking destination!"
     puts "(Enter list to view states again or exit to leave at anytime)"
     input = gets.strip
     index = input.to_i
@@ -40,47 +40,21 @@ class BestHikes::CLI
     if index.between?(1,50) 
       @hikes = BestHikes::Hikes.all
       the_hike = @hikes[index-1]
-      puts "#{the_hike.name}"
+      puts "\n#{the_hike.name}"
       @description = BestHikes::Information.all 
       the_description = @description[index-1]
-      puts "#{the_description.description}"
+      puts "\n#{the_description.description}"
+    elsif input == "list"
+      list_states
     elsif input == "exit"
-      puts "Thanks for playing!"
+      puts "\nThanks for stopping by!"
     else 
-      puts "Invalid entry, please try again."
-    
-#     if input.to_i > 4 #has to come first because it is greater than 0 which is next option
-#       puts "Invalid entry. Please enter your selection. (1-4)"
-#     elsif input.to_i > 0
-#       puts "\n"
-#       the_champs = @champs[input.to_i-1] #creates a local variable to use within this instance method.
-#       puts "#{the_champs.name} won in #{the_champs.year}!"
-#       puts "They beat #{the_champs.opponent}."
-#       puts "Final score was #{the_champs.score}.\n\n"
-#     elsif input == "exit"
-#     puts "Thanks for stopping by!"
-#   elsif input == "list"
-#     list_champs
-#   else 
-#     puts "Invalid entry. please try again."
-#     end
-#   end
-# end
-  def menu
-    puts "\nEnter the number of the state you'd like to view"
-    input = gets.strip
-    index = input.to_i
-    @hikes = BestHikes::Hikes.all
-    the_hike = @hikes[index-1]
-    puts "#{the_hike.name}"
-    @description = BestHikes::Information.all 
-    the_description = @description[index-1]
-    puts "#{the_description.description}"
+      puts "\nInvalid entry, please try again."
     end
-  
-
-  def goodbye
-    puts "Thanks for stopping by!"
   end
 
+  def goodbye
+    puts "Goodbye!"
+  end
+end
 end
